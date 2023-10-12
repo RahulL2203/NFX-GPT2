caef6232aef798e4d58ee70227f20f1fdc5eec47
Rahul Lakkavarapu <rahul_lakkavarapu@Rahuls-MacBook-Air.local>
2023-10-12 19:47:14 +0530

    CRA , Firebase setup and TailwindCSS

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     <h1>Namaste MacBook Air M1</h1>
+     <h1 className='font-bold text-center text-3xl text-green-700'>Namaste MacBook Air M1</h1>
      <h2>Commit Check</h2>
     </div>
   );
