e444c08fce8fbc8cf1258cbbfc7cd53511257466
Rahul Lakkavarapu <rahul_lakkavarapu@Rahuls-MacBook-Air.local>
2023-10-12 13:16:16 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Namaste MacBook Air M1</h1>
+     <h2>Commit Check</h2>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/utils/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAQcaoVPleiTp89hHxyXtCvsYnqXaqy8O0",
+  authDomain: "nf-gpt-9f11b.firebaseapp.com",
+  projectId: "nf-gpt-9f11b",
+  storageBucket: "nf-gpt-9f11b.appspot.com",
+  messagingSenderId: "791473426474",
+  appId: "1:791473426474:web:5b74c79e454ebcad927353",
+  measurementId: "G-CG9B93PQ0C"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
